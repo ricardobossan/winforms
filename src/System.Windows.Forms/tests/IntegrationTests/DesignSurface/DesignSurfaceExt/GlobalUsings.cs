fc38715eb23358c716d6e3fbdcca363a2d6526f9
Ricardo Bossan (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-rbossan@microsoft.com>
2025-02-17 19:44:54 -0300

    Move System.Collections.Immutable package configuration for net481 to eng/Versions.props and add implicit usings to DemoConsole and DesignSurfaceExt

---
+++ src/System.Windows.Forms/tests/IntegrationTests/DesignSurface/DesignSurfaceExt/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+global using System.ComponentModel;
+global using System.ComponentModel.Design;
+global using System.ComponentModel.Design.Serialization;
+global using System.Windows.Forms.Design;
+global using System.Reflection;
+global using System.Collections;
+global using System.Collections.Concurrent;
+global using System.Collections.Immutable;
+
+#if NETFRAMEWORK
+global using System.Drawing;
+global using System.Windows.Forms;
+#endif
+
